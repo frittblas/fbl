0e4cfda8b9677bb20eed36206e443e2b0895ae51
Hans Stromquist <frittblas@gmail.com>
2022-05-15 21:43:07 +0200

    added projects to vs solution

---
+++ game_projects/lotr/Lotr.cpp
@@ -0,0 +1,49 @@
+/*
+*
+*	FBL ENGINE
+*
+*	Cpp_test.cpp
+*
+*	Contains the minimal setup to use C++.
+*	
+*
+*	Hans Strömquist 2022
+*
+*/
+
+#include "../../src/fbl.hpp"
+
+#include <iostream>
+
+
+void fbl_start()
+{
+	
+	// make sure that NAME_OF_ASSETS_FOLDER in fbl.h is set to what you want (default = "assets/")
+
+	fbl_engine_init(960, 540, 60);
+	fbl_set_clear_color(168, 230, 255, 255);
+
+}
+
+void fbl_game_loop()
+{
+
+	if (fbl_get_key_down(FBLK_ESCAPE))
+		fbl_engine_quit();
+
+	if (fbl_get_key_down(FBLK_F9))
+		fbl_set_window_mode(0);
+	if (fbl_get_key_down(FBLK_F10))
+		fbl_set_window_mode(FBL_WINDOW_FULLSCREEN);
+	if (fbl_get_key_down(FBLK_F11))
+		fbl_set_window_mode(FBL_WINDOW_FULLSCREEN_DESKTOP);
+
+}
+
+void fbl_end()
+{
+
+	std::cout<<"Bye!"<<std::endl;
+
+}
