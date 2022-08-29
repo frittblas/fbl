6a86a2c5dbf849c4316aefb76f8fa0013740a492
frittblas <frittblas@gmail.com>
2022-08-29 02:12:24 +0200

    android build system

--- src/engine.h
+++ src/engine.h
@@ -21,6 +21,11 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
+#elif __ANDROID__
+#include "../../SDL.h"
+#include "../../SDL_image.h"
+#include "../../SDL_ttf.h"
+#include "../../SDL_mixer.h"
 #else
 #include <SDL.h>
 #include <SDL_image.h>
